76bc1720bd49a7220cb2df0b819a709541c8c569
JohnMichaels0208 <81570033+JohnMichaels0208@users.noreply.github.com>
2021-04-17 16:37:15 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import random as r
+username = str(input('please enter your name\n'))
+while len(username) < 3:
+    print('your username must be least 3 characters')
+    username = str(input('please enter your name\n'))
+while len(username) > 10:
+    print('your username must be less than 11 characters')
+password = int(input('please enter your password\n'))
+while type(password) == str:
+    print('your password must contain numbers')
+    password = int(input('please enter your password\n'))
+mylist = [
+{username: password}
+]
+list_operations = ['+', '-', '*', '/']
+question = '{}{}{}'.format(r.randint(0, 100), r.choice(list_operations), r.randint(0, 100))
+question1 = ''
+for i in question:
+    question1 += i
+quiz = str(input(question1))
+
+
